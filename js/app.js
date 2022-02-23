4d2c4cc3151af066ec9dca92225e7ed4a2a67283
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-02-23 10:32:13 +0600

    critical error handle

--- js/app.js
+++ js/app.js
@@ -51,23 +51,36 @@ document.getElementById('calculate-cost').addEventListener('click', function () 
 //calculate save & remaining 
 document.getElementById('calculate-save').addEventListener('click', function () {
     const incomeValue = parseFloat(incomeInput.value);
+    const saveInput = document.getElementById('save-value');
+    const saveValue = parseFloat(saveInput.value);
     const balance = incomeValue - getValue()
-    if (balance == NaN) {
+
+    console.log(typeof balance)
+    if (saveValue < 0 || saveValue > 100 || saveInput.value == '') {
+        document.getElementById('total-save').innerText = 'Unexpected value '
         document.getElementById('total-remaining').innerText = 'Unexpected value '
     }
-    if (incomeValue < getValue()) {
+    else if (incomeValue < getValue()) {
         document.getElementById('total-save').innerText = 'Nothing To Save';
         document.getElementById('total-remaining').innerText = 'Earn First';
     }
     else {
-        const totalSave = (incomeValue * 20) / 100;
+        const totalSave = (incomeValue * saveValue) / 100;
         if (balance < totalSave) {
             document.getElementById('total-save').innerText = ' Balace is low nothing to save';
             document.getElementById('total-remaining').innerText = balance;
         }
         else {
-            document.getElementById('total-save').innerText = totalSave;
-            document.getElementById('total-remaining').innerText = balance - totalSave;
+            // 
+            if (isNaN(document.getElementById('total-balance').innerText)) {
+                document.getElementById('total-save').innerText = totalSave;
+                document.getElementById('total-remaining').innerText = 'Balance is nothing'
+            }
+            else {
+
+                document.getElementById('total-save').innerText = totalSave;
+                document.getElementById('total-remaining').innerText = balance - totalSave;
+            }
         }
     }
 })
